ba19eb7858dc70532e8233618fedb21989c1e3ff
FedeIlLeone <dileopolus@virgilio.it>
2023-02-06 19:18:27 +0100

    feat: redo settings and remove unused deps

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,9 +1,8 @@
-import { common, components, util } from "replugged";
+import { components, util } from "replugged";
 import { config } from "./misc";
 import { EmojiStaticExtension } from "./types";
 
-const { React } = common;
-const { Category, Flex, FormItem, Radio, Slider, Switch, Text } = components;
+const { Category, FormItem, Radio, Slider, SwitchItem } = components;
 
 export function Settings() {
   const emojiHideLinks = util.useSetting(config, "emojiHideLinks");
@@ -13,60 +12,42 @@ export function Settings() {
   return (
     <div>
       <Category title="Emoji" note="Emoji spoofer settings">
-        <div style={{ marginBottom: 8 }}>
-          <div style={{ marginBottom: 20 }}>
-            <Flex>
-              <Text variant="text-sm/semibold" style={{ flexGrow: 1 }}>
-                Hide emoji links using Discord spoiler bug (199 trailing spoilers)
-              </Text>
-              <Switch checked={emojiHideLinks.value!} onChange={emojiHideLinks.onChange} />
-            </Flex>
-          </div>
+        <SwitchItem value={emojiHideLinks.value!} onChange={emojiHideLinks.onChange} hideBorder>
+          Hide emoji links using Discord spoiler bug (199 trailing spoilers)
+        </SwitchItem>
 
-          <div style={{ marginTop: 20, marginBottom: 20 }}>
-            <Flex direction={Flex.Direction.VERTICAL}>
-              <Text.Eyebrow color="header-secondary" style={{ marginBottom: 8 }}>
-                Emoji size
-              </Text.Eyebrow>
-              <Flex align={Flex.Align.CENTER} style={{ marginTop: 20, marginBottom: 8 }}>
-                <Slider
-                  {...util.useSetting(config, "emojiSize", 48)}
-                  markers={[16, 24, 32, 40, 48, 56, 64, 80, 96]}
-                  stickToMarkers={true}
-                  defaultValue={48}
-                  minValue={16}
-                  maxValue={96}
-                  onMarkerRender={(e: number) => {
-                    return `${e}px`;
-                  }}
-                />
-              </Flex>
-            </Flex>
-          </div>
-
+        <FormItem title="Emoji Size" style={{ marginBottom: 20 }}>
           <div style={{ marginTop: 20 }}>
-            <Flex direction={Flex.Direction.VERTICAL}>
-              <FormItem>
-                <Text.Eyebrow style={{ marginBottom: 8 }}>Static emoji format</Text.Eyebrow>
-                <Radio
-                  options={[
-                    {
-                      name: "PNG: Better quality",
-                      value: "png",
-                    },
-                    {
-                      name: "WebP: Smaller size",
-                      value: "webp",
-                    },
-                  ]}
-                  value={emojiStaticExtension.value!}
-                  onChange={(e) => emojiStaticExtension.onChange(e.value as EmojiStaticExtension)}
-                  size={Radio.Sizes.MEDIUM}
-                />
-              </FormItem>
-            </Flex>
+            <Slider
+              {...util.useSetting(config, "emojiSize", 48)}
+              markers={[16, 24, 32, 40, 48, 56, 64, 80, 96]}
+              stickToMarkers={true}
+              defaultValue={48}
+              minValue={16}
+              maxValue={96}
+              onMarkerRender={(e: number) => {
+                return `${e}px`;
+              }}
+            />
           </div>
-        </div>
+        </FormItem>
+
+        <FormItem title="Static Emoji Format" style={{ marginBottom: 20 }}>
+          <Radio
+            options={[
+              {
+                name: "PNG: Better quality",
+                value: "png",
+              },
+              {
+                name: "WebP: Smaller size",
+                value: "webp",
+              },
+            ]}
+            value={emojiStaticExtension.value!}
+            onChange={(e) => emojiStaticExtension.onChange(e.value as EmojiStaticExtension)}
+          />
+        </FormItem>
       </Category>
     </div>
   );
